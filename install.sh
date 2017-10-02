008f572a363a360a5f36544e12d2b133546eb057
John Ritsema <john.ritsema@turner.com>
2017-10-02 16:50:27 -0400

    adds install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+app=terraform-provider-harbor
+
+version=$(curl -s https://api.github.com/repos/turnerlabs/$app/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+uname=$(uname)
+suffix=""
+case $uname in
+"Darwin")
+suffix="darwin_amd64"
+;;
+"Linux")
+suffix="linux_amd64"
+esac
+
+url=https://github.com/turnerlabs/$app/releases/download/$version/$suffix
+echo "Getting package $url"
+
+mkdir -p ./terraform.d/plugins/$suffix
+curl -sSLo ./terraform.d/plugins/$suffix/$app $url
+chmod +x ./terraform.d/plugins/$suffix/$app
+
+echo "harbor terraform plugin installed to ./terraform.d/plugins/$suffix"
