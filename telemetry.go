b204f3682575799d1364eb336aa03d784878984f
John Ritsema <john.ritsema@turner.com>
2018-01-26 11:59:53 -0500

    adds user/version fields to telemetry (closes #41) (#43)

--- telemetry.go
+++ telemetry.go
@@ -6,15 +6,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/user"
 	"runtime"
+	"strings"
 )
 
 type metric struct {
-	Source string `json:"source,omitempty"`
-	Action string `json:"action,omitempty"`
-	Error  string `json:"error,omitempty"`
-	OS     string `json:"os,omitempty"`
-	Arch   string `json:"arch,omitempty"`
+	Source  string `json:"source,omitempty"`
+	Action  string `json:"action,omitempty"`
+	Error   string `json:"error,omitempty"`
+	OS      string `json:"os,omitempty"`
+	Arch    string `json:"arch,omitempty"`
+	User    string `json:"user,omitempty"`
+	Version string `json:"version,omitempty"`
 }
 
 const (
@@ -41,21 +45,35 @@ func writeMetricError(action string, err error) {
 	writeMetricErrorString(action, err.Error())
 }
 
+func getVersion() string {
+	//parse version from executable name
+	//e.g.: /some/directory/terraform-provider-harbor_v0.5.0
+	//take everything after last _
+	tmp := strings.Split(os.Args[0], "_")
+	version := tmp[len(tmp)-1]
+	return version
+}
+
 func writeMetricErrorString(action string, err string) {
 
 	// HARBOR_TELEMETRY=0 disables telemetry
 	if setting := os.Getenv("HARBOR_TELEMETRY"); setting != "0" {
 
+		user, e := user.Current()
+		check(e)
+
 		m := metric{
-			Source: "terraform-provider-harbor",
-			Action: action,
-			Error:  err,
-			OS:     runtime.GOOS,
-			Arch:   runtime.GOARCH,
+			Source:  "terraform-provider-harbor",
+			Action:  action,
+			Error:   err,
+			OS:      runtime.GOOS,
+			Arch:    runtime.GOARCH,
+			User:    user.Username,
+			Version: getVersion(),
 		}
 
 		if Verbose {
-			log.Println("posting telemetry data")
+			log.Println("posting telemetry data to: " + telemetryURI)
 		}
 
 		//talk to the server in the background to keep things moving
